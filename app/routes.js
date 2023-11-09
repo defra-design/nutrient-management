3dbd52fe2d42d1b5672e3334d309cc112234da71
Jon Greer <jongreer@Jons-iMac.local>
2023-11-09 12:18:47 +0000

    updated prototype 2

--- app/routes.js
+++ app/routes.js
@@ -8,24 +8,30 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
 
-//Data
+//Import data
 var farm_details = require('./data/farm_details.json');
 var field_details = require('./data/field_details.json');
 
-//set data on index load
+//Index route loads data in application
 router.get('/', function (req, res) { 
-    req.session.data.organic_term = "Organic material"
-    req.session.data.non_organic_term = "Inorganic fertiliser"
+    //data
     req.session.data.field_details = field_details
     req.session.data.chosenfield = null
+
+    // content vars
+    req.session.data.organic_term = "Organic material"
+    req.session.data.non_organic_term = "Inorganic fertiliser"
+    req.session.data.harvest_year = "2023 to 2024"
+    
     // for (var x in field_details) {
     //     console.log ( field_details[x].name )
     // }
     // console.log( `Welcome to ${req.session.data.farm_details.name}` );
+
     res.render('index')
 })
 
-//which filed do you want to create a plan for?
+//which field do you want to create a plan for?
 router.get(/create_plan_handler/, function (req, res) { 
     for ( var y in req.session.data.field_details ) {
         if(req.session.data.field_details[y].reference === req.query.chosenfield) {
@@ -48,12 +54,21 @@ router.get(/plan-type-handler/, function (req, res) {
     }
 })
 
-//do you plan to spread manure?
+//grass or arable?
+router.get(/crop_type_handler/, function (req, res) { 
+    if (req.session.data.field_use == "arable") {
+        res.redirect('q2_crop_group')
+    } else {
+        res.redirect('q2_grass')
+    }
+})
+
+//do you plan to spraed manure?
 router.get(/manure_if_handler/, function (req, res) { 
     if (req.session.data.manure_if == "yes") {
         res.redirect('q5_manure_when')
     } else {
-        res.redirect('ending_handler')
+        res.redirect('check_one')
     }
 })
 
@@ -66,13 +81,13 @@ if (req.session.data.manure_again == "yes") {
     if (req.session.data.plan_type == "new") {
         res.redirect('check_one')
     } else {
-        res.redirect('ending_handler')
+        res.redirect('set_status')
     }
 }
 })
 
 //set the status
-router.get(/ending_handler/, function (req, res) { 
+router.get(/set_status/, function (req, res) { 
     for ( var y in req.session.data.field_details ) {
         if(req.session.data.field_details[y].reference === req.session.data.chosenfield.reference) {
             req.session.data.field_details[y].planStatus = 'Plan complete'
@@ -82,15 +97,28 @@ router.get(/ending_handler/, function (req, res) {
     res.redirect('fields')
 })
 
-//do you plan to spread manure multiple times
-router.get(/fertiliser_again_handler/, function (req, res) { 
-if (req.session.data.fertiliser_again == "yes") {
-    res.redirect('q8_fertiliser_when')
-} else {
-    if (req.session.data.plan_type == "new") {
-        res.redirect('check_two')
+//FERTILISER
+////////////
+
+//do you plan to spraed fertiliser?
+router.get(/fertiliser_if_handler/, function (req, res) { 
+    if (req.session.data.fertiliser_if == "yes") {
+        res.redirect('q8_fertiliser_when')
     } else {
-        res.redirect('ending_handler')
+        res.redirect('check_two')
     }
-}
 })
+
+//do you plan to spread more firtiliser
+router.get(/fertiliser_again_handler/, function (req, res) { 
+    if (req.session.data.fertiliser_again == "yes") {
+        res.redirect('q8_fertiliser_when')
+    } else {
+        if (req.session.data.plan_type == "new") {
+            res.redirect('check_two')
+        } else {
+            res.redirect('set_status')
+        }
+    }
+})
+    

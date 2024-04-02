b6fe32d354cef594b41009e491f91ce93851fc63
Jon Greer <jongreer@Jons-iMac.local>
2024-04-02 13:33:52 +0100

    fixed bug displaying content

--- app/content.js
+++ app/content.js
@@ -2,7 +2,7 @@ var content = {
     organic_term : "Organic material",
     non_organic_term : "Inorganic fertiliser",
     harvest_year : "harvest year 2024",
-    todays_date : "26 March 2024"
+    todays_date : "3 April 2024"
 };
 
 module.exports.content = content;

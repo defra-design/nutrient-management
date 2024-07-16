6e40c68e614c115751cb095c6e4eabe1bf9567d1
Jon Greer <jongreer@Jons-iMac.local>
2024-07-16 14:51:44 +0100

    refactor

--- app/content.js
+++ app/content.js
@@ -2,7 +2,7 @@ var content = {
     organic_term : "Organic material",
     non_organic_term : "Inorganic fertiliser",
     harvest_year : "harvest year 2024",
-    todays_date : "16 July 2024",
+    todays_date : "18 July 2024",
     manure_measurement : "tonnes"
 };
 

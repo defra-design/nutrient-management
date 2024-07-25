0e9f230e24ab4e4cb93eb2c74c25300bf95297bb
Jon Greer <jongreer@Jons-iMac.local>
2024-07-25 15:13:30 +0100

    updated css for grass with 4 tables

--- app/router/routes_prototype_setup.js
+++ app/router/routes_prototype_setup.js
@@ -215,6 +215,22 @@ router.get(/v5_manure_setup_handler/, function (req, res) {
     res.redirect('/version_4/start')
 })
 
+router.get(/v5_grass_setup_handler/, function (req, res) { 
+    req.session.data.show_success_message = false
+    allFunctions.farmSetup(req.session.data.oaktree_farm, req.session.data.plan_2023, req.session.data.plan_2024, 'crops')
+    req.session.data.all_fields = req.session.data.field_list_mvp
+    ///2023 plan setup
+    req.session.data.plan_2023.firstCropReference = 'Grass'
+    req.session.data.plan_2023.firstFieldReferences = [1, 2, 3, 4, 5, 6, 7]
+    req.session.data.plan_2023.firstFields = allFunctions.getMultipleFieldsByReferences(req.session.data.plan_2023.firstFieldReferences, req.session.data.all_fields)
+    ///2024 plan setup
+    req.session.data.plan_2024.firstCropReference = 'Grass'
+    req.session.data.plan_2024.firstFieldReferences = [1, 2, 3, 4, 5, 6 ,7]
+    req.session.data.plan_2024.firstFields = allFunctions.getMultipleFieldsByReferences(req.session.data.plan_2024.firstFieldReferences, req.session.data.all_fields)
+    req.session.data.prototype_version = 'version_4'
+    res.redirect('/version_4/start')
+})
+
 module.exports = router
 
 //246

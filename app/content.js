4212b28651d1b9f93c9a8ef06cefab3dd0d560ad
Jon Greer <jongreer@Jons-iMac.local>
2024-11-12 09:25:30 +0000

    updated start

--- app/content.js
+++ app/content.js
@@ -2,7 +2,7 @@ var content = {
     organic_term : "Organic material",
     non_organic_term : "Inorganic fertiliser",
     harvest_year : "harvest year 2024",
-    todays_date : "18 September 2024",
+    todays_date : "15 November 2024",
     manure_measurement : "tonnes",
     content_loaded  : "Content loaded",
     defoliations  : "Cuts and grazings"

253b51dd0c0196fd3415d3000d3cd51f7440e8b3
Jon Greer <jongreer@Jons-iMac.local>
2024-12-03 13:44:51 +0000

    update to manures

--- app/content.js
+++ app/content.js
@@ -1,8 +1,8 @@
 var content = {
     organic_term : "Organic material",
     non_organic_term : "Inorganic fertiliser",
-    harvest_year : "harvest year 2024",
-    todays_date : "27 November 2024",
+    harvest_year : "harvest year 2025",
+    todays_date : "03 December 2024",
     manure_measurement : "tonnes",
     content_loaded  : "Content loaded",
     defoliations  : "Cuts and grazings"

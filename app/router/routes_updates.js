76322919e68e654a7ce3e1a9c1a55797cf4a907e
Jon Greer <jongreer@Jons-iMac.local>
2025-01-16 11:01:39 +0000

    updated manure update

--- app/router/routes_updates.js
+++ app/router/routes_updates.js
@@ -12,6 +12,13 @@ router.get(/manure_update_handler/, function (req, res) {
     res.redirect(next)
 })
 
+router.get(/manure_update_v6_handler/, function (req, res) {
+    req.session.data.update_type = req.query.update_type
+    console.log(req.session.data.update_type)
+    var next = 'update/manure/fields'
+    res.redirect(next)
+})
+
 // Routers
 
 module.exports = router

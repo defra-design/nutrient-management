3082287fbc9b5b9fb1449cebaa3366861e0f4b91
Jon Greer <jongreer@Jons-iMac.local>
2025-05-21 10:22:06 +0100

    refactor

--- app/content.js
+++ app/content.js
@@ -5,7 +5,8 @@ var content = {
     todays_date : "8 May 2025",
     manure_measurement : "tonnes",
     content_loaded  : "Content is loaded",
-    defoliations  : "Cuts and grazings"
+    defoliations  : "Cuts and grazings",
+    manner_title : "Estimate nutrient supply from organic materials (MANNER-NPK)"
 };
 
 module.exports.content = content;

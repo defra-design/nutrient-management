788f9a688845de22da53c06d37069e407ab2a01b
Jon Greer <jongreer@Jons-iMac.local>
2025-06-12 15:34:02 +0100

    refacor headers

--- app/content.js
+++ app/content.js
@@ -2,11 +2,12 @@ var content = {
     organic_term : "Organic material",
     non_organic_term : "Inorganic fertiliser",
     harvest_year : "harvest year 2025",
-    todays_date : "8 May 2025",
+    todays_date : "12 June 2025",
     manure_measurement : "tonnes",
     content_loaded  : "Content is loaded",
     defoliations  : "Cuts and grazings",
-    manner_title : "Estimate nutrient supply from organic materials (MANNER-NPK)"
+    manner_title : "Estimate nutrient supply from organic materials (MANNER-NPK)",
+    n_loading_title: "Livestock manure nitrogen (N) farm limit"
 };
 
 module.exports.content = content;
